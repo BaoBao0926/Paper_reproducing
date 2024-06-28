5236157fda4c281b831dfeb2e0b33791068bb378
MuyiBao <108871385+BaoBao0926@users.noreply.github.com>
2024-06-28 18:44:03 +0800

    Delete Code directory

--- Code/9.Vision Mamba(Vim)/Vim_main/mamba_1p1p1/mamba_ssm/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "1.1.1"
-
-from Vim_main.mamba_1p1p1.mamba_ssm.ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
-from Vim_main.mamba_1p1p1.mamba_ssm.modules.mamba_simple import Mamba
-from Vim_main.mamba_1p1p1.mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
